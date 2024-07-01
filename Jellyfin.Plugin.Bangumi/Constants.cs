d60e2a4b3d13c724dd447a6d2b9e5e3f3ed0dd66
RandomNamer <zzyatgm@gmail.com>
2024-06-30 20:02:34 -0400

    feat: for me: automatic git hash generation for debug use

--- Jellyfin.Plugin.Bangumi/Constants.cs
+++ Jellyfin.Plugin.Bangumi/Constants.cs
@@ -1,10 +1,12 @@
+using MyNamespace;
+
 namespace Jellyfin.Plugin.Bangumi;
 
 public static class Constants
 {
     public const string ProviderName = "Bangumi";
 
-    public const string PluginName = "Bangumi";
+    public const string PluginName = $"Bangumi {GitInfo.Hash}";
 
     public const string PluginGuid = "41b59f1b-a6cf-474a-b416-785379cbd856";
 
